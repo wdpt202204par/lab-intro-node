14b7dd03fb06f459150ff484ec3deca5076567e1
WilsonF78 <fortes.wilson78@gmail.com>
2022-06-18 16:01:02 +0200

    It 3

--- index.js
+++ index.js
@@ -1,18 +1,29 @@
 class SortedList {
   constructor() {
-    this.items = []
-    this.length = 0
+    this.items = [];
+    this.length = 0;
   }
 
   add(item) {
-    this.items.push(item)
-    this.length++
-    this.items.sort(function(a, b){
-      return a - b
-    })
+    this.items.push(item);
+    this.length++;
+    this.items.sort(function (a, b) {
+      return a - b;
+    });
   }
 
-  get(pos) {}
+  get(pos) {
+    // if (pos > this.items.length - 1) {
+    //   throw new Error("OutOfBounds");
+    // }
+
+    // return this.items[pos];
+
+    if (pos <= this.items.length - 1) {
+      return this.items[pos];
+    }
+    throw new Error("OutOfBounds");
+  }
 
   max() {}
 
@@ -22,6 +33,5 @@ class SortedList {
 
   avg() {}
 }
-
 
 module.exports = SortedList;
